6a722f30d533c167400d278ab838ce8a86d7724a
mwhatters <mwhatters@gmail.com>
2015-08-04 00:47:18 -0700

    redis server

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-$redis = Redis.new(:host => 'localhost', :port=> 6379)
-# if ENV["REDISCLOUD_URL"]
-#     $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
-# end
+# $redis = Redis.new(:host => 'localhost', :port=> 6379)
+if ENV["REDISCLOUD_URL"]
+    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+end
